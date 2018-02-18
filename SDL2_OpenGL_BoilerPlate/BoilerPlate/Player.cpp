41b0c2e4e8acd4fe9988faab77b8de803dffc21c
Arlet Castillo <arletmarcel24@gmail.com>
2018-02-17 23:12:14 -0400

    Modify Colors class

--- SDL2_OpenGL_BoilerPlate/BoilerPlate/Player.cpp
+++ SDL2_OpenGL_BoilerPlate/BoilerPlate/Player.cpp
@@ -13,11 +13,7 @@ Player::Player(float _x, float _y) {
 	angle = 0.0f;
 }
 
-<<<<<<< HEAD
-void Player::wrap(float& vertex, float min, float max) {
-=======
 void Player::Wrap(float& vertex, float min, float max) {
->>>>>>> feature/v0.0.4
 	if (vertex < min) {
 		vertex = max - (min - vertex);
 	}
@@ -26,43 +22,26 @@ void Player::Wrap(float& vertex, float min, float max) {
 	}
 }
 
-<<<<<<< HEAD
-void Player::moveBase(Vector2 _vector) {
-=======
-void Player::Move_forward(){
->>>>>>> feature/v0.0.4
+void Player::Move_forward() {
+	MathUtilities realAngle;
+	base += Vector2(8 * sin(realAngle.degToRad(angle)*-1), 8*cos(realAngle.degToRad(angle)));
+
 	maxWidth = 1136 / 2.0f;
 	halfWidth = -1136 / 2.0f;
 
 	maxHeight = 640 / 2.0f;
 	halfHeight = -640 / 2.0f;
 
-<<<<<<< HEAD
-	base.x += _vector.x; 
-	base.y += _vector.y;
-
-	wrap(base.x, halfWidth, maxWidth);
-	wrap(base.y, halfHeight, maxHeight);
-}
-
-void Player::thruster() {
-=======
-	MathUtilities realAngle;
-	base += Vector2(8 * sin(realAngle.degToRad(angle)*-1), 8*cos(realAngle.degToRad(angle)));
-
 	Wrap(base.x, halfWidth, maxWidth);
 	Wrap(base.y, halfHeight, maxHeight);
 }
 
 void Player::Thruster() {
->>>>>>> feature/v0.0.4
 	glBegin(GL_LINE_LOOP);
 	glVertex2f(-6.0, -7.0);
 	glVertex2f(6.0, -7.0);
 	glVertex2f(0.0, -15.0);
 	glEnd();
-<<<<<<< HEAD
-=======
 }
 
 void Player::Rotate_left() {
@@ -71,17 +50,12 @@ void Player::Rotate_left() {
 
 void Player::Rotate_right() {
 	angle -= 5;
->>>>>>> feature/v0.0.4
 }
 
 void Player::Render() {
 	glLoadIdentity();
 	glTranslatef(base.x, base.y, 0.0f);
-<<<<<<< HEAD
-
-=======
 	glRotatef(angle, 0.0f, 0.0f, 1.0f);
->>>>>>> feature/v0.0.4
 	glBegin(GL_LINE_LOOP);
 	glVertex2f(0.0, 20.0); 
 	glVertex2f(12.0, -10.0); 
@@ -89,27 +63,9 @@ void Player::Render() {
 	glVertex2f(-6.0, -4.0);
 	glVertex2f(-12.0, -10.0);
 
-<<<<<<< HEAD
-	cout << base.x << "," << base.y << endl;
-	glEnd();
-
-	if (activateThruster == true) {
-		thruster();
-	}
-
-	
-	/*
-	 *  glVertex2f(50.0 + base.x, 50.0 + base.y); //primer cuadrante
-	 *  glVertex2f(50.0+base.x, -50.0+base.y); // cuarto cuadrante
-	 *  glVertex2f(-50.0+base.x, -50.0+base.y); //tercer cuadrante
-	 *  glVertex2f(-50.0+base.x, 50.0+base.y); //segundo cuadrante
-	 */
-=======
 	glEnd();
 
 	if (activateThruster == true) {
 		Thruster();
 	}
-
->>>>>>> feature/v0.0.4
 }
--- SDL2_OpenGL_BoilerPlate/BoilerPlate/Player.hpp
+++ SDL2_OpenGL_BoilerPlate/BoilerPlate/Player.hpp
@@ -9,28 +9,13 @@ class  Player{
 	private:
 		//members
 		Vector2 base;
-<<<<<<< HEAD
-=======
 		float angle, mass;
->>>>>>> feature/v0.0.4
 		float maxHeight, maxWidth, halfHeight, halfWidth;
 
 	public:
 		//public member
 		bool activateThruster;
 		//constructors
-<<<<<<< HEAD
-		 Player();
-		 Player(float, float);
-		 //functions
-		 void moveBase(Vector2);
-		 void Render();
-		 void thruster();
-		 void wrap(float&, float, float);
-		 void moveForward();
-		 void rotateLeft();
-		 void rotateRight();
-=======
 		Player();
 		Player(float, float);
 		//functions
@@ -40,7 +25,6 @@ class  Player{
 		void Move_forward();
 		void Rotate_left();
 		void Rotate_right();
->>>>>>> feature/v0.0.4
 };
 #endif // !_PLAYER_H_
 
